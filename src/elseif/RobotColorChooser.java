d3a5975583b7baf8e8ab73e7441952a798607d66
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-18 13:22:25 -0700

    Robot color chooser ip

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -14,20 +14,26 @@ public class RobotColorChooser {
 		//1. Create a new Robot
 		Robot Blackpanther = new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+	String color=JOptionPane.showInputDialog("What color would you like the robot to draw?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+	if(color.equals("red")) {
+		Blackpanther.setPenColor(255,0,0);
+	}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		Blackpanther.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-		Blackpanther.pendown();
+		Blackpanther.penDown();
 		Blackpanther.move(100);
 		Blackpanther.turn(90);
-		
+		Blackpanther.move(100);
+		Blackpanther.turn(90);
+		Blackpanther.move(100);
+		Blackpanther.turn(90);
+		Blackpanther.move(100);
 
 	}
 }
