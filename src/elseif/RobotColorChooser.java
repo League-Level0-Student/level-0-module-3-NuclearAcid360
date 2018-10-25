23edfc9308c35fc192ce091483692b742d0de078
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-25 13:23:37 -0700

    finished robot color chooser and crazy cat lady ip

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -16,11 +16,31 @@ public class RobotColorChooser {
 		//3. Ask the user what color they would like the robot to draw
 	String color=JOptionPane.showInputDialog("What color would you like the robot to draw?");
 		//5. Use an if/else statement to set the pen color that the user requested
+	for(int i=0;i<5;i++) {
 	if(color.equals("red")) {
 		Blackpanther.setPenColor(255,0,0);
 	}
+	else if(color.equals("orange")){
+		Blackpanther.setPenColor(255,119,0);
+	}
+	else if(color.equals("green")) {
+		Blackpanther.setPenColor(26,122,0);
+	}
+	else if(color.equals("brown")) {
+		Blackpanther.setPenColor(104,62,0);	
+	}
+	else if(color.equals("purple")) {
+		Blackpanther.setPenColor(72,12,117);
+	}
+	else if(color.equals("blue")) {
+		Blackpanther.setPenColor(21,41,168);
+	}
+	
         //6. If the user doesn’t enter anything, choose a random color
-
+	else {
+	Blackpanther.setRandomPenColor();
+	}
+	}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
