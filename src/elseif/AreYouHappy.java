bf3a442183de65511c13e3462da3d7b60fb4872f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-13 13:18:56 -0800

    Are you happy ip

--- src/elseif/AreYouHappy.java
+++ src/elseif/AreYouHappy.java
@@ -8,9 +8,15 @@ public static void main(String[] args) {
 
 String answer=JOptionPane.showInputDialog("Are you happy?");
 
-if (answer=="Yes") {
+if (answer.equals("Yes")) {
 JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
 }
 
+if (answer.equals("No")) {
+JOptionPane.showMessageDialog(null,"Do you want to be happy?");
+}
+
+
 }
 }
+
