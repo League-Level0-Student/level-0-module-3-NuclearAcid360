6c1e7cfef35d390e7343c4547e4bb1dc85c4e094
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-20 13:22:21 -0800

    Did a project

---
+++ src/extra/MerryChristmas.java
@@ -0,0 +1,15 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class MerryChristmas {
+public static void main(String[] args) {
+	
+ JOptionPane.showMessageDialog(null, "Merry Christmas!!");
+ JOptionPane.showMessageDialog(null, "We Hope you have a good time with family and friends!");
+ String present = JOptionPane.showInputDialog("We help people get presents, what would you like!!");
+ JOptionPane.showMessageDialog(null, "We will try to get you a");
+ JOptionPane.showMessageDialog(null, present);
+ JOptionPane.showMessageDialog(null, "We hope you have a merry christmas");
+}
+}
