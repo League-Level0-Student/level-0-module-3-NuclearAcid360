66285a4ebc9cdc15e4e53d134f0f2f604f73b8a3
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-07 13:24:40 -0800

    Finished are you happy. Horoscope ip

--- src/elseif/AreYouHappy.java
+++ src/elseif/AreYouHappy.java
@@ -4,24 +4,24 @@ import javax.swing.JOptionPane;
 
 public class AreYouHappy {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-String answer=JOptionPane.showInputDialog("Are you happy?");
+		String answer = JOptionPane.showInputDialog("Are you happy?");
 
-if (answer.equals("Yes")) {
-JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+		if (answer.equals("Yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+		}
+
+		else if (answer.equals("No")) {
+			answer = JOptionPane.showInputDialog("Do you want to be happy?");
+
+			if (answer.equals("No")) {
+				JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+			}
+			else if (answer.equals("Yes")) {
+				JOptionPane.showMessageDialog(null, "Change Something");
+			}
+		}
+	}
+
 }
-
-if (answer.equals("No")) {
-JOptionPane.showInputDialog("Do you want to be happy?");
-
-if (answer.equals("No")) {
-	JOptionPane.showMessageDialog(null, ");
-	
-}
-}
-
-
-}
-}
-
